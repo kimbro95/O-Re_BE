f8b42d4cfafeb7c61a3e2e441345f1441166beee
bro <gudgudgud714@naver.com>
2023-10-27 09:36:46 +0900

    dotenv 패키지 추가 및 config 파일 추가

---
+++ src/config/index.js
@@ -0,0 +1,15 @@
+const dotenv = require("dotenv");
+
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
+const envFound = dotenv.config();
+if (envFound.error) {
+  throw new Error("⚠ Couldn't find .env file");
+}
+
+module.exports = {
+  port: parseInt(process.env.PORT, 10) || 3002,
+  api: {
+    prefix: "/api",
+  },
+};
--- src/server.js
+++ src/server.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const config = require("./config");
 
 async function startServer() {
   const app = express();
-  const PORT = 3002;
-  app.listen(PORT, () => {
-    console.log(`Start Server ${PORT}`);
+  const PORT = config.port;
+
+  app.listen(PORT, (error) => {
+    try {
+      if (error) throw error;
+      console.log(`> ✨Ready on http://localhost:${PORT}`);
+    } catch (error) {
+      console.log("Error Server");
+      process.exit(1);
+    }
   });
 }
 
