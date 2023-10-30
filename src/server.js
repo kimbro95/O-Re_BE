ac478fd251eb6a921a01269c5ad6bbfe46926aab
bro <gudgudgud714@naver.com>
2023-10-30 14:38:53 +0900

    node-schedule 패키지 추가 및 06시 박스오피스 데이터 호출 스케줄러 실행

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 import express from "express";
+import { scheduleJob } from "node-schedule";
+
 import config from "./config/index.js";
 import loader from "./loaders/index.js";
-
+import { dateFormat } from "./api/utils/date.js";
+import { writeBoxOfficeJson } from "./api/utils/boxOffice.js";
 const startServer = async () => {
   const app = express();
   const PORT = config.port;
@@ -12,6 +15,12 @@ const startServer = async () => {
     try {
       if (error) throw error;
       console.log(`> ✨Ready on http://localhost:${PORT}`);
+      // 매일 06시 boxOfffice 스케줄러 실행
+      scheduleJob("0 0 6 * * *", async () => {
+        const nowDate = dateFormat(new Date());
+        console.log(`${nowDate} boxOffice.json 스케줄러 실행`);
+        await writeBoxOfficeJson();
+      });
     } catch (error) {
       console.log("Error Server");
       process.exit(1);
